4ad2cf0f9b8c515f13ace9b08f842a8385da8051
Roger Lo <sobadrush@icloud.com>
2021-10-14 19:36:48 +0800

    Eureka-Client 註冊 電影微服務

--- microservice_movie/src/main/java/com/cathaybk/movie/MovieApplication.java
+++ microservice_movie/src/main/java/com/cathaybk/movie/MovieApplication.java
@@ -2,6 +2,7 @@ package com.cathaybk.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
  * 電影微服務
  */
 @SpringBootApplication
+@EnableEurekaClient // 啟動Eureka客戶端
 public class MovieApplication {
 
 	public static void main(String[] args) {
