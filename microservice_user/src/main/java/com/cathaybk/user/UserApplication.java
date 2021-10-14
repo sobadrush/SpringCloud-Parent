836213def3394f2ce28aefff45542d96aa90ae98
Roger Lo <sobadrush@icloud.com>
2021-10-14 19:37:04 +0800

    Eureka-Client 註冊 使用者微服務

--- microservice_user/src/main/java/com/cathaybk/user/UserApplication.java
+++ microservice_user/src/main/java/com/cathaybk/user/UserApplication.java
@@ -2,11 +2,13 @@ package com.cathaybk.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 用戶微服務
  */
 @SpringBootApplication
+@EnableEurekaClient // 啟動Eureka客戶端
 public class UserApplication {
 
 	public static void main(String[] args) {
